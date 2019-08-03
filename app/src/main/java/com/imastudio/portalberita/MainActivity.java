e126184d6c4a65943e25fb38269152dfc770d6d6
imastudiojkt <info@imastudio.co.id>
2019-08-03 10:51:03 +0700

    update untuk mas chandra

--- app/src/main/java/com/imastudio/portalberita/MainActivity.java
+++ app/src/main/java/com/imastudio/portalberita/MainActivity.java
@@ -1,22 +1,57 @@
 package com.imastudio.portalberita;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 
+import com.imastudio.portalberita.model.ResponseBerita;
+import com.imastudio.portalberita.networking.MyRetrofitClient;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.listBerita)
     RecyclerView listBerita;
+    private boolean status;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        //get data from db using retrofit
+        getDataBerita();
 
+
+    }
+
+    private void getDataBerita() {
+        //tampilkan loading
+        final ProgressDialog loading = ProgressDialog.show(this, "Proses get data",
+                "loading...");
+
+        MyRetrofitClient.getInstaceRetrofit().getdataBerita().enqueue(new Callback<ResponseBerita>() {
+            //ketika berhasil menampilkan data json
+            @Override
+            public void onResponse(Call<ResponseBerita> call, Response<ResponseBerita> response) {
+                if (response.isSuccessful()) {
+                    //menghilangkan loading
+                    loading.dismiss();
+                    status = response.body().isStatus();
+                }
+            }
+
+            //ketika gagal menampilkan data json
+            @Override
+            public void onFailure(Call<ResponseBerita> call, Throwable t) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/imastudio/portalberita/adapter/PortalBeritaAdapter.java
+++ app/src/main/java/com/imastudio/portalberita/adapter/PortalBeritaAdapter.java
@@ -2,14 +2,18 @@ package com.imastudio.portalberita.adapter;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class PortalBeritaAdapter extends
         RecyclerView.Adapter<PortalBeritaAdapter.ViewHolder> {
+
     @NonNull
     @Override
     public PortalBeritaAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        LayoutInflater inflater = ()
+        View v=
         return null;
     }
 
@@ -23,7 +27,7 @@ public class PortalBeritaAdapter extends
         return 0;
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    public class ViewHolder extends RecyclerView.ViewHolder {
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
         }
